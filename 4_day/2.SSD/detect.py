414aa69cd1f70ea3a06eaba9e0531fa713cb2f68
kim jung in <37010399+imguru-mooc@users.noreply.github.com>
2023-10-19 02:47:22 +0900

    Add files via upload

--- 4_day/2.SSD/detect.py
+++ 4_day/2.SSD/detect.py
@@ -96,7 +96,7 @@ def detect(original_image, min_score, max_overlap, top_k, suppress=None):
 
 
 if __name__ == '__main__':
-    img_path = '/media/ssd/ssd data/VOC2007/JPEGImages/000001.jpg'
+    img_path = '/content/VOCdevkit/VOC2007/JPEGImages/000001.jpg'
     original_image = Image.open(img_path, mode='r')
     original_image = original_image.convert('RGB')
     detect(original_image, min_score=0.2, max_overlap=0.5, top_k=200).show()
